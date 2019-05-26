303d85540a4e8be07af52cf63b4766fef35e9063
leon <linwe2012@163.com>
2019-05-27 00:16:03 +0800

    buffer manager bug fix

--- Database0/buffer-manager.cc
+++ Database0/buffer-manager.cc
@@ -1,13 +1,154 @@
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
 #endif // _MSC_VER
 
 #include "buffer-manager.h"
+#include <fstream>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <io.h>
+/*
+class FStream {
+public:
+	FStream(fs::path p) {
+		Open(p);
+	}
 
-#include <sstream>
-#include <exception>
+	FStream() {}
+	FStream(const FStream&) = delete;
 
+	const FStream& operator=(const FStream&) = delete;
 
+	void Open(fs::path p) {
+		ios.open(p, std::ios::binary | std::ios::in | std::ios::out);
+		if (!ios.is_open()) {
+			ios.clear();
+			throw std::ios_base::failure("Unbale o open file");
+		}
+	}
+
+	void NewFile(fs::path p) {
+		std::ofstream os(p);
+		os.close();
+		ios.open(p);
+	}
+	void Write(const void* data, size_t size) {
+		ios.write(reinterpret_cast<const char*>(data), size);
+	}
+	void SeekPut(size_t pos, int way) {
+		ios.seekp(pos, way);
+	}
+
+	void SeekGet(size_t pos, int way) {
+		ios.seekg(pos, way);
+	}
+
+	
+	void Read(void* buf, size_t size) {
+		ios.read(reinterpret_cast<char*>(buf), size);
+	}
+
+	void Flush() {
+		ios.flush();
+	}
+
+	size_t TellGet() {
+		return ios.tellg();
+	}
+
+	size_t TellPut() {
+		return ios.tellp();
+	}
+
+private:
+	std::fstream ios;
+};*/
+
+class FStream {
+public:
+	FStream(fs::path p) {
+		Open(p);
+	}
+
+	FStream() {}
+	FStream(const FStream&) = delete;
+
+	const FStream& operator=(const FStream&) = delete;
+
+	void Open(fs::path p) {
+		fd = open(p.string().c_str(), O_BINARY | O_RDWR, S_IREAD | S_IWRITE);
+
+		if (fd == -1) {
+			throw std::ios_base::failure("Unbale o open file");
+		}
+	}
+
+	void NewFile(fs::path p) {
+		fd = open(p.string().c_str(), O_BINARY | O_CREAT | O_RDWR, S_IREAD | S_IWRITE);
+		if (fd == -1) {
+			throw std::ios_base::failure("Unbale o open file");
+		}
+	}
+	void Write(const void* data, size_t size) {
+		auto res = write(fd, data, size);
+		if (res == -1) {
+			int a = EBADF;
+			int b = ENOSPC;
+			int c = EINVAL;
+			int err = errno;
+			throw std::ios_base::failure("Unable to write");
+		}
+		if (res != size) {
+			throw std::ios_base::failure("Not All written to file");
+		}
+	}
+
+	void Read(void* buf, size_t size) {
+		auto res = read(fd, buf, size);
+		if (res != size) {
+			throw std::ios_base::failure("Read failed");
+		}
+	}
+	void SeekPut(size_t pos, int way) {
+
+		auto res = lseek(fd, pos, SEEK_SET);
+		if (way == std::ios::beg) {
+			if (res != pos) {
+				throw std::ios_base::failure("Fail to seek");
+			}
+		}
+	}
+
+	void SeekGet(size_t pos, int way) {
+		auto res = lseek(fd, pos, SEEK_SET);
+		if (way == std::ios::beg) {
+			if (res != pos) {
+				throw std::ios_base::failure("Fail to seek");
+			}
+		}
+	}
+
+	void Flush() {
+
+	}
+
+	size_t TellGet() {
+		return tell(fd);
+	}
+
+	size_t TellPut() {
+		return tell(fd);
+	}
+
+	~FStream() {
+		close(fd);
+	}
+private:
+	int fd = -1;
+	size_t pos;
+};
 
 bool BufferManager::IsOpened(const char* path)
 {
@@ -18,6 +159,9 @@ bool BufferManager::IsOpened(const char* path)
 	return true;
 }
 
+FStream& GetStream(BufferManager::FileInfo& finfo) {
+	return *reinterpret_cast<FStream*>(finfo.fd);
+}
 
 void TestFileOk(FILE* f, const std::string& abs_path) {
 	if (f == nullptr) {
@@ -38,18 +182,21 @@ FileId BufferManager::OpenFile(const char* path)
 	}
 
 	FileInfo finfo;
-	finfo.fd = fopen(canon.string().c_str(), "rb+");
+	finfo.fd = new FStream(path);
+	
+	// finfo.fd = fopen(canon.string().c_str(), "r+");
 	// finfo.fd = open(canon.string().c_str(), O_BINARY | O_CREAT, S_IREAD | S_IWRITE);
 	finfo.abs_path = canon.string();
 
-	TestFileOk(finfo.fd, finfo.abs_path);
+	// TestFileOk(finfo.fd, finfo.abs_path);
 	if (file_infos_.size() >= FileId::kMax) {
 		throw std::overflow_error("Too many files!");
 	}
 
 	finfo.id = FileId(static_cast<FileId::IdType>(file_infos_.size()));
 	loaded_files_[canon] = finfo.id;
-	fread(&finfo.header, sizeof(finfo.header), 1, finfo.fd);
+	GetStream(finfo).Read(&finfo.header, sizeof(finfo.header));
+	// fread(&finfo.header, sizeof(finfo.header), 1, finfo.fd);
 	file_infos_.push_back(finfo);
 	
 	return finfo.id;
@@ -57,10 +204,16 @@ FileId BufferManager::OpenFile(const char* path)
 
 FileId BufferManager::NewFile(const char* path)
 {
-	fs::path canon = fs::canonical(path);
-	if (fs::exists(canon)) {
+	FileInfo finfo;
+	fs::path canon = path;
+	if (fs::exists(path)) {
 		std::string what = "File:" + canon.string() + " already exits";
 		throw std::ios_base::failure(what);
+	}
+	else{
+		// finfo.fd = fopen(canon.string().c_str(), "w+");
+		finfo.fd = new FStream();
+		canon = fs::canonical(path);
 	}
 	
 	auto itr = loaded_files_.find(canon);
@@ -68,15 +221,18 @@ FileId BufferManager::NewFile(const char* path)
 		return itr->second.id;
 	}
 
-	FileInfo finfo;
-	finfo.fd = fopen(canon.string().c_str(), "wb+");
+	
 	finfo.abs_path = canon.string();
-	TestFileOk(finfo.fd, finfo.abs_path);
+	// TestFileOk(finfo.fd, finfo.abs_path);
 
 	Page* page = GetEmptyPage(0, 0);
 
-	fwrite(page, Page::kPageSize, 1, finfo.fd);
-	fseek(finfo.fd, 0, SEEK_SET);
+	auto& ios = GetStream(finfo);
+	ios.NewFile(path);
+	ios.Write(page, Page::kPageSize);
+	
+	// fwrite(page, Page::kPageSize, 1, finfo.fd);
+	// fseek(finfo.fd, 0, SEEK_SET);
 	
 	if (file_infos_.size() >= FileId::kMax) {
 		throw std::overflow_error("Too many files!");
@@ -111,23 +267,28 @@ Page* BufferManager::AutoFetchPage(UniquePage unipage)
 
 	Page* page = new Page;
 	page->piggyback = ppb;
+
+	auto& ios = GetStream(finfo);
+	ios.SeekGet(unipage.page * Page::kPageSize, std::ios::beg);
+	ios.Read(page, Page::kPageSize);
 	
-	fseek(finfo.fd, unipage.page * Page::kPageSize, SEEK_SET);
+	// auto seel_res = fseek(finfo.fd, unipage.page * Page::kPageSize, SEEK_SET);
 	// lseek(finfo.fd, unipage.page * Page::kPageSize, SEEK_SET);
+	/*
 	auto res = fread(page, Page::kPageSize, 1, finfo.fd);
 	if (res != 1) {
 		std::ostringstream ss;
 		ss << "Fail to read page " << unipage.page << " in file: " << finfo.abs_path;
-		if (feof(finfo.fd)) {
-			ss << ". Unexpected end of file.";
+		if (ferror(finfo.fd)) {
+			ss << ". error code(" << errno << ").";
 		}
 		else {
-			ss << ". error code(" << errno << ").";
+			ss << ". Unexpected end of file.";
 		}
 		
 		clearerr(finfo.fd);
 		throw std::ios_base::failure(ss.str());
-	}
+	}*/
 
 	pages_[unipage] = page;
 	return page;
@@ -135,20 +296,20 @@ Page* BufferManager::AutoFetchPage(UniquePage unipage)
 
 void BufferManager::FlushPageToDisk(Page* page, PageId pid) {
 	auto piggy = static_cast<PagePiggyback*>(page->piggyback);
-	int fd = piggy->finfo.id;
-
-	fseek(piggy->finfo.fd, pid * Page::kPageSize, SEEK_SET);
-	fwrite(page, Page::kPageSize, 1, piggy->finfo.fd);
-	fflush(piggy->finfo.fd);
+	auto& ios = GetStream(piggy->finfo);
+	ios.SeekPut(pid * Page::kPageSize, std::ios::beg);
+	ios.Write(page, Page::kPageSize);
+	ios.Flush();
 	page->is_dirty = false;
 }
 
 void BufferManager::FlushFileHeaderToDisk(FileId fid)
 {
 	auto& finfo = file_infos_[fid];
-	fseek(finfo.fd, 0, SEEK_SET);
-	fwrite(&finfo.header, sizeof(finfo.header), 1, finfo.fd);
-	fflush(finfo.fd);
+	auto& ios = GetStream(finfo);
+	ios.SeekPut(0, std::ios::beg);
+	ios.Write(&finfo.header, sizeof(finfo.header));
+	ios.Flush();
 }
 
 void BufferManager::UnloadPage(UniquePage unipage)
@@ -173,10 +334,10 @@ BufferManager::~BufferManager()
 		UnloadPage((itr++)->first);
 	}
 
+	/*
 	for (auto finfo : file_infos_) {
-		fclose(finfo.fd);
 		finfo.fd = nullptr;
-	}
+	}*/
 	
 	// STLs are expcted to do the rest jobs
 }
@@ -198,7 +359,7 @@ PageId BufferManager::AllocatePageAfter(FileId fid, PageId prev)
 
 			// update previous page's valude pointing to self
 			int next = prev_page->header.next;
-			prev_page->header.next = prev - current;
+			prev_page->header.next = current - prev;
 			prev_page->is_dirty = true;
 
 			// get next page
@@ -227,10 +388,20 @@ PageId BufferManager::AllocatePageAfter(FileId fid, PageId prev)
 		}
 		PageId current = static_cast<PageId::IdType>(finfo.header.num_pages);
 
-		fseek(finfo.fd, 0, SEEK_END);
 
-		fwrite(populate_adjust_page(current), Page::kPageSize, 1, finfo.fd);
-
+		long end_of_file = current * Page::kPageSize;
+		auto& ios = GetStream(finfo);
+		
+		
+		auto page = populate_adjust_page(current);
+		ios.SeekPut(end_of_file, std::ios::beg);
+		ios.Write(page, Page::kPageSize);
+		
+		// auto after_write_size = ios.TellPut();
+		// if (after_write_size != (current.id + 1) * Page::kPageSize) {
+		// 	throw std::ios_base::failure("Something went wrong when writing to file");
+		// }
+		
 		return current;
 	}
 
@@ -286,8 +457,9 @@ Page::Header BufferManager::GetPageHeader(FileId fid, PageId pid)
 	}
 
 	Page::Header header;
-	fseek(file_infos_[fid].fd, pid * Page::kPageSize, SEEK_SET);
-	fread(&header, sizeof(header), 1, file_infos_[fid].fd);
+	auto& ios = GetStream(file_infos_[fid]);
+	ios.SeekGet(pid * Page::kPageSize, std::ios::beg);
+	ios.Read(&header, sizeof(header));
 	return header;
 }
 
@@ -297,6 +469,8 @@ void BufferManager::DeletePage(Page* page)
 	delete piggy;
 	delete page;
 }
+
+
 
 
 
--- Database0/test_buffm.cc
+++ Database0/test_buffm.cc
@@ -13,30 +13,40 @@ int main()
 	test.push_back("for (int i = 0; i < 10; ++i) {");
 	test.push_back("++itr;");
 	test.push_back("itr.Insert(10.0 * i);");
-
-
+	srand(0);
+	constexpr int fill_up = Page::kDiscretionSpace / sizeof(double) + 100;
 	// write data
 	if (!fs::exists(db_file_name)) {
 		db_file = bm.NewFile(db_file_name);
 
 		// allocate first page for column A
-		PageId column_A = bm.AllocatePageAfter(db_file, 0);
+		PageId column_A_1 = bm.AllocatePageAfter(db_file, 0);
 		PageId column_Var = bm.AllocatePageAfter(db_file, 0);
-
+		PageId column_A_2 = bm.AllocatePageAfter(db_file, column_A_1);
 		// get an iterator on the page
-		auto itr = bm.GetPage<double>(db_file, column_A);
+		auto itr = bm.GetPage<double>(db_file, column_A_1);
 
 		// char* 是一个特殊的迭代器，表示变长的数据，边长数据理论上可以存放任何类型的数据，但会占更多空间
-		auto v = bm.GetPage<char*>(db_file, column_Var);
-		for (int i = 0; i < 10; ++i) {
-			itr.Insert(10.0 * i);
+		// auto v = bm.GetPage<char*>(db_file, column_Var);
+		auto last_page = &itr.page();
+		for (int i = 0; i < fill_up; ++i) {
+			if (!itr.Insert(/* (double)rand() / RAND_MAX*/ double(i))) {
+				++itr;
+				itr.Insert(double(i));
+			}
+			
+			if (&itr.page() != last_page) {
+				std::cerr << "We've just walk into another page!!" << std::endl;
+				last_page = &itr.page();
+				getchar();
+			}
 			++itr;
 		}
 
-		for (int i = 0; i < 10; ++i) {
-			v.Insert(test[i % test.size()]);
-			++v;
-		}
+		//for (int i = 0; i < 10; ++i) {
+		//	v.Insert(test[i % test.size()]);
+		//	++v;
+		//}
 	}
 	// read data
 	else {
@@ -49,14 +59,16 @@ int main()
 		//TODO 计划在 单个Page 上实现二分查找
 		auto itr = bm.GetPage<double>(db_file, 1);
 		auto v = bm.GetPage<char*>(db_file, 2);
-		for (int i = 0; i < 10; ++i) {
-			std::cout << "row " << i << ": " << *itr << std::endl;
+		auto itr2 = bm.GetPage<double>(db_file, 3);
+		for (int i = 0; i < fill_up; ++i) {
+			
+			if (*itr != double(i) /*(double)rand() / RAND_MAX*/) {
+				std::cout << "row " << i << " fail" << std::endl;
+			}
 			++itr;
-		}
-
-		for (int i = 0; i < 10; ++i) {
-			std::cout << "row " << i << ": " << v.AsString() << std::endl;
-			++v;
+			if (itr.IsEndPage()) {
+				++itr;
+			}
 		}
 		
 		getchar();
